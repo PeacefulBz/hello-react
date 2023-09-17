858a6add96c4911fa0810273e05624752a14f514
PeacefulBz <6411130049@mut.ac.th>
2023-09-17 13:26:09 +0700

    UserAndEmp

--- src/Components/UserAndEmp.tsx
+++ src/Components/UserAndEmp.tsx
@@ -3,9 +3,14 @@ import "./UserAndEmp.css";
 
 function UserAndEmp() {
   return (
-    <div className="UserBox">
-      <div className="UserIcon">
+    <div className="content">
+      <div className="content-left">
         <h1>User</h1>
+        <button className="content-button">User Button</button>
+      </div>
+      <div className="content-right">
+        <h1>Employee</h1>
+        <button className="content-button">Employee Button</button>
       </div>
     </div>
   );
