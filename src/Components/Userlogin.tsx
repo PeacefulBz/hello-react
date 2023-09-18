fbf69e2ce4efb178d496f13257effd9deb315fcc
PeacefulBz <6411130049@mut.ac.th>
2023-09-18 09:57:31 +0700

    Update

--- src/Components/Userlogin.tsx
+++ src/Components/Userlogin.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+import "./Userlogin.css";
 
 function Userlogin() {
   return (
     <>
-      <div>UserLogin</div>
+      <div className="container">
+        <h2>Login</h2>
+        <div className="icon">
+          <i className="fas fa-user"></i>
+        </div>
+        <form className="login-form" action="#" method="post">
+          <div className="form-group">
+            <label className="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+              required
+            ></input>
+          </div>
+          <div className="form-group">
+            <label className="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              required
+            ></input>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Login"></input>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
