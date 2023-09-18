6613fe12313b92c741280129edf252a5ee998059
PeacefulBz <6411130049@mut.ac.th>
2023-09-18 10:01:58 +0700

    Update Userlogin

--- src/Components/Userlogin.tsx
+++ src/Components/Userlogin.tsx
@@ -4,36 +4,38 @@ import "./Userlogin.css";
 function Userlogin() {
   return (
     <>
-      <div className="container">
-        <h2>Login</h2>
-        <div className="icon">
-          <i className="fas fa-user"></i>
+      <div className="bodylogin">
+        <div className="logincontainer">
+          <h2>Login</h2>
+          <div className="icon">
+            <i className="fas fa-user"></i>
+          </div>
+          <form className="login-form" action="#" method="post">
+            <div className="form-group">
+              <label className="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                required
+              ></input>
+            </div>
+            <div className="form-group">
+              <label className="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+              ></input>
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Login"></input>
+            </div>
+          </form>
         </div>
-        <form className="login-form" action="#" method="post">
-          <div className="form-group">
-            <label className="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Enter your username"
-              required
-            ></input>
-          </div>
-          <div className="form-group">
-            <label className="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              required
-            ></input>
-          </div>
-          <div className="form-group">
-            <input type="submit" value="Login"></input>
-          </div>
-        </form>
       </div>
     </>
   );
